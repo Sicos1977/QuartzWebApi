53ec80c129ec22bc6a000436866bcd6dd11c8fa2
Kees van Spelde <keesvanspelde@hotmail.com>
2022-06-19 19:05:12 +0200

    Added loggers

---
+++ QuartzWebApi/Loggers/Console.cs
@@ -0,0 +1,12 @@
+﻿namespace QuartzWebApi.Loggers
+{
+    /// <summary>
+    ///     Writes log information to the console
+    /// </summary>
+    public class Console: Stream
+    {
+        public Console() : base(System.Console.OpenStandardOutput())
+        {
+        }
+    }
+}
